975af514c18fa7fa21e8a0b8f738ce67de6d9b3a
Bhavaz <47023310+Bhavaz@users.noreply.github.com>
2019-02-09 16:33:42 -0600

    c

--- ICP3/GitHubUserFinder/script.js
+++ ICP3/GitHubUserFinder/script.js
@@ -1,55 +1,71 @@
 function getGithubInfo(user) {
+    console.log(user)
+    var url_1="https://api.github.com/users/"+user;
+    console.log(url_1);
+    $('#usrname').html('');
+    $('#usrid').html('');
+    $('.avatar').html('');
+    $('.information').html('');
     //1. Create an instance of XMLHttpRequest class and send a GET request using it. The function should finally return the object(it now contains the response!)
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            showUser(JSON.parse(this.responseText));
-        }
-        else if (this.readyState == 4) {
-            noSuchUser(user)
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 ) {
+            if (this.status == 200) {
+                $('#usrname').text('Username:');
+                $('#usrid').text('ID');
+                //$('.avatar').html('<img src=' + myObject["avatar_url"] + ' alt="img">')
+                //$('.information').html('<a href=' + myObject['url'] + '> Link to profile </a>');
+                var myObject = JSON.parse(this.responseText);
+                console.log(myObject)
+                $('#usrname').append(myObject['login']);
+                $('#usrid').append(myObject['id'])
+                $('.avatar').html('<img src=' + myObject["avatar_url"] + ' alt="img">')
+                $('.information').html('<a href=' + myObject['html_url'] + '> Link to profile </a>');
+            }
+            else
+            {
+                $('#usrname').text('Invalid User');
+            }
         }
     };
-    xhttp.open("GET", "https://api.github.com/users/" + user, true);
+
+    xhttp.open("GET",url_1,false);
     xhttp.send();
+
+
 }
 
 function showUser(user) {
-
+    console.log(1)
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
-
-    $("#header").text(user.login)
-    $("#avatar").append('<img src="'+user.avatar_url+'" width="100px" height="100px"/>')
-    $("#information").append('<a href="'+user.html_url+'">Github Url</a></br><a href="'+user.repos_url+'">Repo Url</a>')
 
 }
 
 function noSuchUser(username) {
     //3. set the elements such that a suitable message is displayed
 
-    $("#header").text("no such profile")
+    console.log(2)
+
 }
 
 
 $(document).ready(function(){
     $(document).on('keypress', '#username', function(e){
         //check if the enter(i.e return) key is pressed
-        $("#header").html("")
-        $("#avatar").html("")
-        $("#information").html("")
         if (e.which == 13) {
             //get what the user enters
             username = $(this).val();
             //reset the text typed in the input
             $(this).val("");
             //get the user's information and store the respsonse
-            getGithubInfo(username);
+            response = getGithubInfo(username);
             //if the response is successful show the user's details
-            //  if (response.status == 200) {
-            //      showUser(JSON.parse(response.responseText));
-            //else display suitable message
-            //  } else {
-            //     noSuchUser(username);
-            //  }
+            if (response.status == 200) {
+                showUser(JSON.parse(response.responseText));
+                //else display suitable message
+            } else {
+                noSuchUser(username);
+            }
         }
     })
 });
